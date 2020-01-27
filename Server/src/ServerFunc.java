2a88abf5e4c8fa4ab0ccde5126477f03c5aa0660
David Eriksson <cryslacks@live.se>
2020-01-27 14:25:08 +0100

    Basic login system added
    A basic but effective login system has been added to the server.

--- Server/src/ServerFunc.java
+++ Server/src/ServerFunc.java
@@ -5,19 +5,16 @@ import java.net.Socket;
 import org.json.JSONObject;
 
 public class ServerFunc {
+	public static boolean debugMode;
+
 	static public boolean sendMsg(ObjectOutputStream s, JSONObject j) {
 		try {
 			s.writeObject(j.toString());
 			s.flush();
-			System.out.println("sending info");
 		} catch (IOException e) {
 			System.out.println("ERR_WRITE_MSG");
 			return false;
 		}
 		return true;
-	}
-
-	public static String generateKey() {
-		return "0xdeadbeef";
 	}
 }
