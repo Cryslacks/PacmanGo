7328ae941962b38a124a53e12c03c4397f3ff490
Fredde <freddelindahl2@gmail.com>
2020-02-26 10:50:58 +0100

    SAVE_MAP and CH support for save map

--- Server/src/DatabaseHandler.java
+++ Server/src/DatabaseHandler.java
@@ -53,56 +53,42 @@ public class DatabaseHandler{
 	}
 	public static MapValues loadMap(int id){
 		ResultSet rs = db.query("SELECT * FROM maps WHERE map_id = '"+id+"'");
-		MapValues mapInfo = null;
+		MapData mapData = null;
 		int[][] sendAdj;
 		try {
 			while(rs.next()){
 				
 				String mapName = rs.getString("name");
-				JSONObject j = new JSONObject(rs.getString("pos_mat"));   //>>>>>>ÄNDRA TIL MAP INFO<<<<<
-				Coordinate[] coords = new Coordinate[j.getJSONArray("pos_mat").length()];
-				for(int i = 0; i < j.getJSONArray("pos_mat").length(); i++) {
-		            coords[i] = new Coordinate(j.getJSONArray("pos_mat").getJSONObject(i).getJSONArray("coord").getDouble(0),j.getJSONArray("pos_mat").getJSONObject(i).getJSONArray("coord").getDouble(0));
-		            if(ServerFunc.debugMode){
-		            	System.out.println("LoadMap: coord_nr "+i+":");
-		            	System.out.println("\t x="+coords[i].getCoord()[0]);            
-			            System.out.println("\t y="+coords[i].getCoord()[1]);
-					
-		            }
-		            
-				}
-				sendAdj = new int[j.getJSONArray("pos_mat").length()][j.getJSONArray("pos_mat").length()];
-				for(int i = 0; i < j.getJSONArray("adj_mat").length(); i++) {
-					for(int q = 0; q < j.getJSONArray("adj_mat").getJSONArray(i).length(); q++) {
-						sendAdj[i][q] = j.getJSONArray("adj_mat").getJSONArray(i).getInt(q);
-					}
-					
-					
-		            if(ServerFunc.debugMode){
-		            	System.out.println("LoadMap: coord_nr "+i+":");
-		            	System.out.println("\t x="+coords[i].getCoord()[0]);            
-			            System.out.println("\t y="+coords[i].getCoord()[1]);
-					
-		            }
-		            
-				}
-				mapInfo = new MapValues(mapName, sendAdj, coords);
+				JSONObject data = new JSONObject(rs.getString("map_data"));   //>>>>>>ÄNDRA TIL MAP INFO<<<<<
 				
-				
-		
-			}
-				
+				mapData = new MapData(data, mapName);
+			}		
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return mapInfo;
+		return mapData;
+	}
 	
-	}
 	public static void loadCoin(){
-		
+		//DO we even NEED?
 	}
-	public static void saveMap(String name, double[] adj_mat, double[] adj_pos){
-		//query(INSERT .. .. .. INTO maps) pro tip arrays.toString();
+	public static boolean saveMap(JSONObject j){
+		ResultSet rs = db.query("SELECT * FROM maps WHERE name = '"+j.getString("MapName")+"'");
+			
+			try {
+				if(rs.next())
+					return false;
+				else{
+					String tempName = j.getString("MapName");
+					j.remove("MapName");
+					db.query("INSERT INTO maps (mapname, data) VALUES ('"+tempName+"', '"+j.toString()+"')");
+					return true;
+				}
+			} catch (SQLException e) {
+				return false;
+			}
+		
+		
 	}
 	
 	
